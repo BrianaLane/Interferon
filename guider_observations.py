2c238dea76682050fa08b7c2bf7e31050affee45
Briana Lane Indahl <blindahl@astro.as.utexas.edu>
2021-07-09 14:25:44 -0500

    generalize star finding functions

--- guider_observations.py
+++ guider_observations.py
@@ -11,30 +11,19 @@ import pandas as pd
 import numpy as np
 import glob
 import datetime as dt
-import matplotlib.pyplot as plt
 import warnings
 
-from astropy import coordinates as coords
+import image_utils
+
 from astropy.wcs import WCS
-from astropy import units as u
-from astropy.coordinates import SkyCoord, Angle
 from astropy.io import fits
 from astropy.stats import sigma_clipped_stats
-from astropy.modeling import models, fitting
-import astropy.visualization as av
 from astropy.utils.exceptions import AstropyWarning
-
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore")
-    from photutils import DAOStarFinder
-    
-from astroquery.sdss import SDSS
-from photutils import CircularAperture, aperture_photometry, CircularAnnulus
 
 
 class guider_observations():
     def __init__(self, guider_path, guider_as_per_pix=0.51):
-        
+
         print('BUILD guider observation: [GUIDER]')
 
         self.guider_path = guider_path
@@ -55,8 +44,9 @@ class guider_observations():
             self.guider_df.at[g, 'obs_datetime'] = obs_dt
             self.guider_df.at[g, 'exptime(s)'] = hdr_g['EXPTIME']
             hdu.close()
-  
-    # guider_name (int/str): can be either guider index in guider_df or filename
+
+    # guider_name (int/str): can be either guider index in
+    # guider_df or filename
     def inspect_guider_frame(self, guider_name, vmin=None, vmax=None):
         if isinstance(guider_name, int):
             if guider_name < len(self.guider_df):
@@ -84,21 +74,7 @@ class guider_observations():
             im_wcs = WCS(hdr)
         hdu.close()
 
-        fig = plt.figure(figsize=(6, 6))
-        ax = fig.add_subplot(1, 1, 1, projection=im_wcs)
-
-        norm = av.ImageNormalize(dat, interval=av.ZScaleInterval(),
-                                 stretch=av.SqrtStretch())
-
-        if (vmin is None) or (vmax is None):
-            ax.imshow(dat, cmap='Greys', norm=norm)
-        else:
-            ax.imshow(dat, cmap='Greys', norm=norm, vmin=vmin, vmax=vmax)
-
-        ax.set_ylabel('RA', fontsize=25)
-        ax.set_xlabel('DEC', fontsize=25)
-        ax.tick_params(axis='both', which='major', labelsize=15)
-        plt.show()
+        image_utils.plot_frame(dat, im_wcs, vmin=vmin, vmax=vmax)
 
     def find_guide_stars(self, guide_ind, star_thres=10., num_bright_stars=10,
                          star_fwhm=8.0, plot_guide_frame=False):
@@ -106,39 +82,23 @@ class guider_observations():
         hdu = fits.open(self.guider_df.iloc[guide_ind]['filename'])
         g_dat = hdu[0].data
 
-        mean, median, std = sigma_clipped_stats(g_dat, sigma=3.0)
-        daofind = DAOStarFinder(fwhm=star_fwhm, threshold=star_thres*std,
-                                peakmax=64000, exclude_border=True,
-                                brightest=num_bright_stars)
-
-        sources = daofind(g_dat)
-        for col in sources.colnames:
-            sources[col].info.format = '%.8g'  # for consistent table output
-        sources_df = sources.to_pandas()
-
-        if plot_guide_frame:
-            fig, ax = plt.subplots(1,1,figsize=(10, 10))
-            ax.imshow(g_dat, cmap='gray', vmin=1310, vmax=1400, origin='lower')
-            for i in range(len(sources_df)):
-                aperture = CircularAperture((sources_df.iloc[i]['xcentroid'],
-                                             sources_df.iloc[i]['ycentroid']),r=star_fwhm)
-
-                aperture.plot(color='red', lw=2.5)
-            plt.show()
+        sources_df = image_utils.find_stars(g_dat, star_thres=star_thres,
+                                            num_bright_stars=num_bright_stars,
+                                            star_fwhm=star_fwhm,
+                                            plot_sources=plot_guide_frame)
 
         hdu.close()
 
         return sources_df.copy()
 
-    def identify_stars(self, guide_ind):
+    def identify_guide_stars(self, guide_ind):
         gframe_df = self.guider_df.iloc[guide_ind]
         hdu = fits.open(gframe_df['filename'])
         dat = hdu[0].data
         hdr = hdu[0].header
 
-        g_coords = coords.SkyCoord(hdr['RA'], hdr['DEC'])
-        sources_tab = SDSS.query_crossid(g_coords, photoobj_fields=['modelMag_g', 'modelMag_i'])
-        sources_df = soures_tab.to_pandas()
+        sources_df = image_utils.identify_stars(dat, hdr)
+
         hdu.close()
         return sources_df
 
@@ -150,76 +110,21 @@ class guider_observations():
 
         # divide guider image by exptime to get counts per second
         guide_et = self.guider_df.iloc[guide_ind]['exptime(s)']
-        g_dat_perS = g_dat/guide_et #counts per second
+        g_dat_perS = g_dat/guide_et  # counts per second
 
         # remove background (approx bias) level from guider image
         mean, median, std = sigma_clipped_stats(g_dat_perS, sigma=3.0)
         g_dat_perS_noBKGD = g_dat_perS - median
 
-        for s in range(len(sources_df)):
-            so = sources_df.iloc[s]
-            xp, yp = (int(so['xcentroid']), int(so['ycentroid']))
-            # this defines half width/height of square cutout
-            off = 10
+        sources_df = image_utils.measure_star_params(g_dat_perS_noBKGD, sources_df,
+                                                     plot_star_cutouts=plot_star_cutouts)
 
-            # makes sure it is not too close to the edge to create cutout
-            if (xp > (off-1)) and (yp > (off-1)):
-                # build cutout image 
-                im = g_dat_perS_noBKGD[yp-off:yp+off, xp-off:xp+off]
-                # create array of x and y coordinates of cutout for the fitter
-                y_grid, x_grid = np.mgrid[:np.shape(im)[0], :np.shape(im)[1]]
-                # finds center for new coodinates of cutout
-                x_cen, y_cen = (xp - (xp-off), yp - (yp-off))
-
-                g = models.Gaussian2D(amplitude=so['peak']-mean, x_mean=x_cen,
-                                      y_mean=y_cen, x_stddev=1.0, y_stddev=1.0)
-                gfit = fitting.LevMarLSQFitter()
-
-                with warnings.catch_warnings():
-                    warnings.simplefilter('ignore', AstropyWarning)
-                    # fit 2D Guassian model to cutout image
-                    fit_g = gfit(g, x_grid, y_grid, im)
-
-                sd = np.average([fit_g.x_stddev.value, fit_g.y_stddev.value])
-                xcen_fit_s, ycen_fit_s = (fit_g.x_mean.value, fit_g.y_mean.value)
-                fwhm_fit = 2.*np.sqrt(2.*np.log(2))*sd
-                flux_fit = 2*np.pi*fit_g.amplitude.value*fit_g.x_stddev.value*fit_g.y_stddev.value
-                mag_fit = -2.5*np.log10(flux_fit)
-
-                sources_df.at[s, 'xcentroid_fit'] = xcen_fit_s-x_cen+xp
-                sources_df.at[s, 'ycentroid_fit'] = ycen_fit_s-y_cen+yp
-                sources_df.at[s, 'fwhm(arcseconds)'] = fwhm_fit*self.guider_ps
-                sources_df.at[s, 'flux_fit'] = flux_fit
-                sources_df.at[s, 'mag_fit'] = mag_fit
-
-                if plot_star_cutouts:
-                    plt.imshow(im,cmap='gray',origin='lower')
-                    plt.scatter(x_cen, y_cen, marker='x', color='white')
-                    fit_x, fit_y = (xcen_fit_s, ycen_fit_s)
-                    plt.scatter(fit_x, fit_y, marker='x', color='red')
-                    aperture = CircularAperture((fit_x, fit_y), r=fwhm_fit)
-                    aperture.plot(color='red', lw=1.5)
-                    plt.text(1, 2, 'fwhm:'+str(np.round(fwhm_fit, 2)),
-                             fontsize=12, color='white')
-                    plt.text(1, 4, 'peak:'+str(np.round(fit_g.amplitude.value, 2)),
-                             fontsize=12, color='white')
-                    plt.text(1, 6, 'mag:'+str(np.round(mag_fit, 2)),
-                             fontsize=12, color='white')
-                    plt.title(s)
-                    plt.show()
-
-            # excludes if too close to edge to make cutout 
-            else:
-                sources_df.at[s, 'xcentroid_fit'] = np.NaN
-                sources_df.at[s, 'ycentroid_fit'] = np.NaN
-                sources_df.at[s, 'fwhm(arcseconds)'] = np.NaN
-                sources_df.at[s, 'flux_fit'] = np.NaN
-                sources_df.at[s, 'mag_fit'] = np.NaN
+        sources_df['fwhm(arcseconds)'] = sources_df['fwhm(pixels)']*self.guider_ps
 
         hdu.close()
         return sources_df.copy()
 
-    def flag_stars(self, sources_df, fwhm_lim=(0.5, 10), mag_lim=10):
+    def flag_stars(sources_df, fwhm_lim=(0.5, 10), mag_lim=10):
         for i in range(len(sources_df)):
             if (np.isnan(sources_df.iloc[i]['mag_fit'])) or (np.isnan(sources_df.iloc[i]['mag_fit'])):
                 sources_df.at[i, 'bad_flag'] = True
@@ -242,17 +147,19 @@ class guider_observations():
             source_ex = self.find_guide_stars(guide_ind, star_thres=star_thres,
                                               num_bright_stars=num_bright_stars,
                                               star_fwhm=star_fwhm)
-            if len(source_ex)>1:
+            if len(source_ex) > 1:
                 source_fit = self.measure_guide_star_params(guide_ind,
                                                             source_ex)
+
                 source_fit = self.flag_stars(source_fit.copy(),
-                                             fwhm_lim=fwhm_lim,
-                                             mag_lim=mag_lim)
+                                                    fwhm_lim=fwhm_lim,
+                                                    mag_lim=mag_lim)
+
                 source_fit['guide_ind'] = guide_ind
 
                 good_sources_df = source_fit[source_fit['bad_flag'] == False].reset_index(drop=True)
 
-                if len(good_sources_df)>1:
+                if len(good_sources_df) > 1:
                     return good_sources_df, guide_ind
                 else:
                     continue
---
+++ image_utils.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  8 16:13:46 2021
+
+@author: Briana
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from astropy.io import fits
+import astropy.visualization as av
+from astropy.modeling import models, fitting
+from astropy import units as u
+from astropy.coordinates import SkyCoord, Angle
+from astropy import coordinates as coords
+from astropy.stats import sigma_clipped_stats
+from astropy.utils.exceptions import AstropyWarning
+import warnings
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore")
+    from photutils import DAOStarFinder
+
+from astroquery.sdss import SDSS
+from photutils import CircularAperture, aperture_photometry, CircularAnnulus
+
+
+def plot_subframe(ax, frame, vmin=None, vmax=None, c_map='Greys'):
+
+    norm = av.ImageNormalize(frame, interval=av.ZScaleInterval(),
+                             stretch=av.SqrtStretch())
+
+    if (vmin is None) or (vmax is None):
+        ax.imshow(frame, cmap=c_map, origin='lower', norm=norm)
+    else:
+        ax.imshow(frame, cmap=c_map, origin='lower', norm=norm,
+                  vmin=vmin, vmax=vmax)
+
+    ax.set_xlabel('RA', fontsize=25)
+    ax.set_ylabel('DEC', fontsize=25)
+    ax.tick_params(axis='both', which='major', labelsize=15)
+
+
+def plot_frame(frame, wcs=None, vmin=None, vmax=None, c_map='Greys',
+               save=False, outfile=None):
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(1, 1, 1, projection=wcs)
+
+    plot_subframe(ax, frame, vmin=vmin, vmax=vmax, c_map=c_map)
+
+    if save:
+        try:
+            plt.savefig(outfile)
+        except:
+            print('Invaild save name for plot')
+
+    plt.show()
+
+
+def cube_to_image_hdr(col_frame, wcs_hdr):
+    hdu_new = fits.PrimaryHDU(col_frame, header=wcs_hdr)
+    return hdu_new
+
+
+def find_stars(frame, star_thres=10., num_bright_stars=10,
+               star_fwhm=8.0, plot_sources=False):
+
+    mean, median, std = sigma_clipped_stats(frame, sigma=3.0)
+    daofind = DAOStarFinder(fwhm=star_fwhm, threshold=star_thres*std,
+                            peakmax=64000, exclude_border=True,
+                            brightest=num_bright_stars)
+
+    sources = daofind(frame)
+    for col in sources.colnames:
+        sources[col].info.format = '%.8g'  # for consistent table output
+    sources_df = sources.to_pandas()
+
+    if plot_sources:
+        fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+        plot_subframe(ax, frame, vmin=1310, vmax=1400)
+        for i in range(len(sources_df)):
+            aperture = CircularAperture((sources_df.iloc[i]['xcentroid'],
+                                         sources_df.iloc[i]['ycentroid']),
+                                        r=star_fwhm)
+
+            aperture.plot(color='red', lw=2.5)
+        plt.show()
+
+    return sources_df.copy()
+
+
+def identify_stars(frame, hdr):
+
+    g_coords = coords.SkyCoord(hdr['RA'], hdr['DEC'])
+    sources_tab = SDSS.query_crossid(g_coords,
+                                     photoobj_fields=['modelMag_g',
+                                                      'modelMag_i'])
+    sources_df = sources_tab.to_pandas()
+
+    return sources_df
+
+
+def measure_star_params(frame, sources_df, plot_star_cutouts=False):
+
+    for s in range(len(sources_df)):
+        so = sources_df.iloc[s]
+        xp, yp = (int(so['xcentroid']), int(so['ycentroid']))
+        # this defines half width/height of square cutout
+        off = 10
+
+        # makes sure it is not too close to the edge to create cutout
+        if (xp > (off-1)) and (yp > (off-1)):
+            # build cutout image
+            im = frame[yp-off:yp+off, xp-off:xp+off]
+            # create array of x and y coordinates of cutout for the fitter
+            y_grid, x_grid = np.mgrid[:np.shape(im)[0], :np.shape(im)[1]]
+            # finds center for new coodinates of cutout
+            x_cen, y_cen = (xp - (xp-off), yp - (yp-off))
+
+            g = models.Gaussian2D(amplitude=so['peak'], x_mean=x_cen,
+                                  y_mean=y_cen, x_stddev=1.0, y_stddev=1.0)
+
+            gfit = fitting.LevMarLSQFitter()
+
+            with warnings.catch_warnings():
+                warnings.simplefilter('ignore', AstropyWarning)
+                # fit 2D Guassian model to cutout image
+                fit_g = gfit(g, x_grid, y_grid, im)
+
+            sd = np.average([fit_g.x_stddev.value, fit_g.y_stddev.value])
+            xcen_fit_s, ycen_fit_s = (fit_g.x_mean.value, fit_g.y_mean.value)
+            fwhm_fit = 2.*np.sqrt(2.*np.log(2))*sd
+            flux_fit = 2*np.pi*fit_g.amplitude.value*fit_g.x_stddev.value*fit_g.y_stddev.value
+            mag_fit = -2.5*np.log10(flux_fit)
+
+            sources_df.at[s, 'xcentroid_fit'] = xcen_fit_s-x_cen+xp
+            sources_df.at[s, 'ycentroid_fit'] = ycen_fit_s-y_cen+yp
+            sources_df.at[s, 'fwhm(pixels)'] = fwhm_fit
+            sources_df.at[s, 'flux_fit'] = flux_fit
+            sources_df.at[s, 'mag_fit'] = mag_fit
+
+            if plot_star_cutouts:
+                plt.imshow(im, cmap='gray', origin='lower')
+                plt.scatter(x_cen, y_cen, marker='x', color='white')
+                fit_x, fit_y = (xcen_fit_s, ycen_fit_s)
+                plt.scatter(fit_x, fit_y, marker='x', color='red')
+                aperture = CircularAperture((fit_x, fit_y), r=fwhm_fit)
+                aperture.plot(color='red', lw=1.5)
+                plt.text(1, 2, 'fwhm:'+str(np.round(fwhm_fit, 2)),
+                         fontsize=12, color='white')
+                plt.text(1, 4, 'peak:'+str(np.round(fit_g.amplitude.value, 2)),
+                         fontsize=12, color='white')
+                plt.text(1, 6, 'mag:'+str(np.round(mag_fit, 2)),
+                         fontsize=12, color='white')
+                plt.title(s)
+                plt.show()
+
+        # excludes if too close to edge to make cutout
+        else:
+            sources_df.at[s, 'xcentroid_fit'] = np.NaN
+            sources_df.at[s, 'ycentroid_fit'] = np.NaN
+            sources_df.at[s, 'fwhm(pixels)'] = np.NaN
+            sources_df.at[s, 'flux_fit'] = np.NaN
+            sources_df.at[s, 'mag_fit'] = np.NaN
+
+    return sources_df.copy()
